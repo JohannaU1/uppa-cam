9a5d0e6faf66ccc65a53c726b32082ac630c702f
JohannaU1 <94961913+JohannaU1@users.noreply.github.com>
2022-10-03 02:03:57 +0200

    updates

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,13 +5,14 @@ import pentaxK70 from "../Picture/PentaxK70.jpg"
 import canon from "../Picture/canonEos2000D.jpg";
 const HomePage = () => {
     return (
-     
-     <div className="main-content">
+    // main-content = parent element
+     <div style={{backgroundColor: "Green"}} className="main-content">
       
-    <div  className="main-info">
-    <img  src={canon} alt="Canonkamera"  />
+    {/* main-info just for image box */}
+    <div style={{backgroundColor: "yellow"}} className="main-info">
+    <img   />
     
-    <div className="btn-pos">
+    <div style={{backgroundColor: "red"}} className="btn-pos">
       <button className="btn-prio">Buy</button>
       <button className="btn-secondery">Read</button>
     </div>
@@ -21,42 +22,42 @@ const HomePage = () => {
     </div>
      
      
-     
-      <div  style={{backgroundColor: "rgb(243, 235, 235)"}} className="main-info">¨
+    
+      <div  style={{backgroundColor: "purple"}} className="main-info">¨
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
      </div>
      
-     <div className="order-aside"><Aside /></div>
+     <div style={{backgroundColor: "brown"}} className="order-aside"><Aside /></div>
     
       
       
-     <div className="product-item1">¨
-     <img  src={canon} alt="Canonkamera"  />
+     <div style={{backgroundColor: "lightblue"}} className="product-item1">¨
+     <img    />
      
-     <div className="btn-pos-mini">
+     <div style={{backgroundColor: "yellowgreen"}} className="btn-pos-mini">
      <button className="btn-prio-mini">Buy</button>
      <button className="btn-secondary-mini">Read More</button>
      </div>
 
      </div>
 
-     <div  className="product-item2">¨
-     <img  src={pentaxK70} alt="Canonkamera"  />
+     <div style={{backgroundColor: "lightblue"}} className="product-item2">¨
+     <img    />
      
-     <div className="btn-pos-mini">
+     <div style={{backgroundColor: "yellowgreen"}} className="btn-pos-mini">
      <button className="btn-prio-mini">Buy</button>
      <button className="btn-secondary-mini">Read More</button>
      </div>
      
      </div>
 
-     <div className="product-item3">¨
-     <img  src={canon} alt="Canonkamera"  />
+     <div style={{backgroundColor: "lightblue"}} className="product-item3">¨
+     <img   />
 
-     <div className="btn-pos-mini">
+     <div style={{backgroundColor: "yellowgreen"}} className="btn-pos-mini">
      <button className="btn-prio-mini">Buy</button>
      <button className="btn-secondary-mini">Read More</button>
      </div>
